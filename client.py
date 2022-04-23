1a0acd1aefa9b539b6cee61e6e869a535eac58fd
narupo <narupo.glab@gmail.com>
2022-04-23 17:40:41 +0900

    add comment

--- client.py
+++ client.py
@@ -21,19 +21,23 @@ class Client(tk.Tk):
         self.msg_row = tk.Frame(self)
         self.msg_row.pack(side=tk.TOP, expand=True, fill=tk.X)
 
+        # メッセージ入力用のエントリー（入力欄）
         self.msg = ttk.Entry(self.msg_row)
         self.msg.pack(side=tk.LEFT, expand=True, fill=tk.X)
 
+        # メッセージ送信ボタン
         self.send_msg_btn = ttk.Button(
             self.msg_row,
             text='Send',
-            command=self.send_msg,
+            command=self.send_msg,  # クリックするとこれが呼ばれる
         )
         self.send_msg_btn.pack(side=tk.LEFT)
 
+        # ソケットの初期化
         self.init_send_sock()
         self.init_broadcast_sock()
 
+        # ブロードキャスト受信用のワーカースレッドを起動
         self.broadcast_worker_thread = threading.Thread(target=self.broadcast_worker, daemon=True)
         self.broadcast_worker_thread.start()
 
@@ -42,8 +46,12 @@ class Client(tk.Tk):
         self.text.see(tk.END)
          
     def init_send_sock(self):
+        # TCPソケットを作成
         self.send_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # アドレスに接続する
         self.send_sock.connect((socket.gethostname(), MESSAGE_PORT))
+
         self.log('send socket connected\n')
 
     def init_broadcast_sock(self):
@@ -52,14 +60,14 @@ class Client(tk.Tk):
         self.log('broadcast socket connected\n')
 
     def send_msg(self):
-        if not self.send_sock:
-            return
-
-        msg = self.msg.get()
-        msg = msg.encode()
-        self.send_sock.send(msg)
+        # メッセージを送信する
+        msg = self.msg.get()  # エントリーから文字列を得る
+        msg = msg.encode()  # 文字列をバイト列に変換
+        self.send_sock.send(msg)  # サーバーに送信
 
     def broadcast_worker(self):
+        # サーバーからブロードキャストを受信する
+        
         while True:
             time.sleep(1)
 
--- server.py
+++ server.py
@@ -15,23 +15,29 @@ class Server(tk.Tk):
         super().__init__()
         self.title('TkChat Server')
 
+        # スクロールバー付きのテキストエリア
         self.text = ScrolledText(self)
         self.text.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
 
+        # ソケットの初期化
         self.init_recv_worker_sock()
         self.init_broadcast_worker_sock()
+
+        # これにブロードキャスト先のソケットが保存される
         self.broadcast_socks = []
 
+        # メッセージ受信用スレッド
         self.recv_worker_thread = threading.Thread(target=self.recv_worker, daemon=True)
         self.recv_worker_thread.start()
 
+        # ブロードキャスト用スレッド
         self.broadcast_worker_thread = threading.Thread(target=self.broadcast_worker, daemon=True)
         self.broadcast_worker_thread.start()
 
     def log(self, msg):
-        self.text.insert(tk.END, msg)
-        self.text.see(tk.END)
-         
+        self.text.insert(tk.END, msg)  # 末尾に挿入
+        self.text.see(tk.END)  # 末尾を見る
+
     def init_recv_worker_sock(self):
         # TCPソケットを作る
         self.recv_worker_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -50,13 +56,18 @@ class Server(tk.Tk):
         self.log('broadcast worker socket is ready\n')
 
     def recv_worker(self):
+        # メッセージ受信用
+        # クライアントから接続を受け付ける
+
         while True:
             time.sleep(1)
 
+            # 接続を受付けてソケットを作成する
             self.log('recv: accept...\n')
             client_sock, addr = self.recv_worker_sock.accept()
-            client_sock.settimeout(10)
+            client_sock.settimeout(10)  # タイムアウト設定
 
+            # 接続されたソケットを渡してワーカースレッドを起動
             th = threading.Thread(
                 target=self.recv_client_worker,
                 args=(client_sock, ),
@@ -66,21 +77,34 @@ class Server(tk.Tk):
             self.log('accept new client socket for receive\n')
 
     def broadcast_worker(self):
+        # ブロードキャスト用
+        # クライアントから接続を受け付ける
+
         while True:
             time.sleep(1)
 
             self.log('broadcast: accept...\n')
             client_sock, addr = self.broadcast_worker_sock.accept()
-            client_sock.settimeout(3)
+
+            # ブロードキャスト用のタイムアウトは短めに設定
+            # 長くブロックすると接続切れのソケットがあった場合に
+            # ブロードキャストが途中で止まる場合がある
+            client_sock.settimeout(3) 
 
             # 接続してきたクライアントのソケットをリストに追加する
+            # このリストを参照してブロードキャストを行う
             self.broadcast_socks.append(client_sock)
+
             self.log('accept new client socket for broadcast\n')
 
     def recv_client_worker(self, client_sock):
+        # メッセージ受信用
+        # 接続済みのクライアントとのソケットを使ってメッセージを受信する
+
         while True:
             time.sleep(1)
 
+            # 1024バイトまでデータを受信する
             try:
                 msg = client_sock.recv(1024)
             except ConnectionResetError:
@@ -88,20 +112,23 @@ class Server(tk.Tk):
                 self.remove_broadcast_socket(client_sock)
                 break
             except socket.timeout:
+                # タイムアウト
                 continue
 
-            msg = msg.decode()
+            msg = msg.decode()  # バイト列を文字列に変換
             self.log(f'msg: {msg}\n')
 
-            # 各クライアントへメッセージをブロードキャスト
+            # 各クライアントへ受信したメッセージをブロードキャスト
             self.broadcast(msg)
 
     def remove_broadcast_socket(self, sock):
+        # sockをbroadcast_socksから除外する
         self.broadcast_socks = list(
             filter(lambda s: id(s) != id(sock), self.broadcast_socks)
         )
 
     def broadcast(self, msg):
+        # メッセージを各クライアントへブロードキャストする
         msg = msg.encode()
 
         for sock in self.broadcast_socks:
